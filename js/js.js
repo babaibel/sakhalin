bad37c7dbf8f4180d0edba0a04fa7083a3928af9
Babai <31babaev@gmail.com>
2017-11-30 16:59:19 +0300

    main page

--- js/js.js
+++ js/js.js
@@ -1,47 +1,12 @@
-// var $W = $(window),
-// 	$D = $(document),
-// 	$H = $('html'),
-// 	$B = $('body');
-
 $(function() {
-
 	$('.js-fish-slider').slick({
 		dots: false,
 		arrows: false,
-		infinite: false,
+		infinite: true,
 		speed: 300,
 		slidesToShow: 5,
-		slidesToScroll: 1,
-		responsive: [
-		{
-		  breakpoint: 1024,
-		  settings: {
-		    slidesToShow: 3,
-		    slidesToScroll: 3,
-		    infinite: true,
-		    dots: true
-		  }
-		},
-		{
-		  breakpoint: 600,
-		  settings: {
-		    slidesToShow: 2,
-		    slidesToScroll: 2
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-		    slidesToShow: 1,
-		    slidesToScroll: 1
-		  }
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-		]
+		slidesToScroll: 5
 	});
-
 });
 
 $(function() {
@@ -54,28 +19,30 @@ $(function() {
 });
 
 $(function() {
+	var $gallery = $('.js-gallery');
+	if(!$gallery.length) return;
 
-	$('.js-gallery-bigslider').slick({
+	var $galleryBig = $gallery.find('.js-gallery-bigslider'),
+		$gallerySmall = $gallery.find('.js-gallery-smallslider'),
+		$gallerySmallItems = $gallerySmall.find('.js-gallery-smallslider-item');
+
+	$galleryBig.slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
 	  arrows: false,
 	  fade: true
 	});
 
-});
-$(function() {
-	// $('.gallery-bigslider').magnificPopup({
-	// 	delegate: '.bigslider__item',
-	// 	type: 'image',
-	// 	gallery:{
-	// 	    enabled:true
-	// 	  }
-	// 	 // other options
-	// });
+	$gallerySmallItems.on('click', function(){
+		var $this = $(this),
+			number = $this.index();
 
-	$('.gallery-bigslider').each(function() { // the containers for all your galleries
+		$galleryBig.slick('slickGoTo', number)
+	});
+
+	$galleryBig.each(function() {
 	    $(this).magnificPopup({
-	        delegate: 'a', // the selector for gallery item
+	        delegate: 'a',
 	        type: 'image',
 	        gallery: {
 	          enabled:true
@@ -84,155 +51,47 @@ $(function() {
 	});
 });
 
-// $(function() {
+$(function() {
+	var $tourSilderWr = $('.js-tours-slider-wr');
+	if(!$tourSilderWr.length) return;
 
-// 	$('.js-popup-modal').magnificPopup({
-// 		type: 'inline',
-// 		preloader: false,
-// 		focus: '#username',
-// 		showCloseBtn: false,
-// 		mainClass: 'popup-modal-overlay'
-// 	});
+	var $tourSilder = $tourSilderWr.find('.js-tours-slider'),
+		$tourSilderNav = $tourSilderWr.find('.js-tours-slider-controls'),
+		$tourSilderList = $tourSilderWr.find('.js-tours-slider-list'),
+		$tourSilderListItems = $tourSilderList.find('.js-tours-slider-list-item');
 
-// 	$(document).on('click', '.popup-modal-dismiss', function (e) {
-// 		e.preventDefault();
-// 		$.magnificPopup.close();
-// 	});
-// });
+	$tourSilder.on('init', function(slick){
+	  $tourSilderListItems.eq(0).addClass('active');
+	});
 
-// $(function() {
+	$tourSilder.slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		appendDots: $tourSilderNav,
+		fade: true
+	});
 
-// 	$('.js-form').each(function(){
-// 		// Объявляем переменные (форма и кнопка отправки)
-// 		var form = $(this),
-// 		    btn = form.find('.blue-btn');
+	$tourSilder.on('beforeChange', function(event, slick, currentSlide,nextSlide){
+	  $tourSilderListItems.removeClass('active');
+	  $tourSilderListItems.eq(nextSlide).addClass('active');
+	});
 
-// 		// Добавляем каждому проверяемому полю, указание что поле пустое
-// 		form.find('.rfield').addClass('empty_field');
+	$tourSilderListItems.on('click', function(){
+		var $this = $(this),
+			number = $this.index();
 
-// 		// Функция проверки полей формы
-// 		function checkInput(){
-// 			form.find('.rfield').each(function(){
-// 				if($(this).val() != ''){
-// 					  // Если поле не пустое удаляем класс-указание
-// 					$(this).removeClass('empty_field');
-// 				} else {
-// 					  // Если поле пустое добавляем класс-указание
-// 					$(this).addClass('empty_field');
-// 				}
-// 			});
-// 		}
+		$tourSilderListItems.removeClass('active');
+		$this.addClass('active');
 
-// 		// Функция подсветки незаполненных полей
-// 		function lightEmpty(){
-// 			form.find('.empty_field').addClass('error');
-// 			// Через полсекунды удаляем подсветку
-// 			setTimeout(function(){
-// 			form.find('.empty_field').removeClass('error');
-// 			},500);
-// 		}
+		$tourSilder.slick('slickGoTo', number)
+	});
+});
 
-// 		// Проверка в режиме реального времени
-// 		setInterval(function(){
-// 			// Запускаем функцию проверки полей на заполненность
-// 			checkInput();
-// 			// Считаем к-во незаполненных полей
-// 			var sizeEmpty = form.find('.empty_field').size();
-// 			// Вешаем условие-тригер на кнопку отправки формы
-// 			if(sizeEmpty > 0){
-// 			if(btn.hasClass('disabled')){
-// 			  return false
-// 			} else {
-// 				btn.addClass('disabled')
-// 			}
-// 			} else {
-// 				btn.removeClass('disabled')
-// 			}
-// 		}, 500);
-
-
-// 		// Событие клика по кнопке отправить
-// 		btn.click(function(){
-// 		  if($(this).hasClass('disabled')){
-// 		    // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
-// 			lightEmpty();
-// 			return false
-// 		  } else {
-// 				form.submit(function (){
-
-// 					var data = form.serialize();
-
-// 					$.ajax({ 
-// 						// инициaлизируeм ajax зaпрoс
-// 						type: 'POST', // oтпрaвляeм в POST фoрмaтe, мoжнo GET
-// 						url: 'mail.php', // путь дo oбрaбoтчикa, у нaс oн лeжит в тoй жe пaпкe
-// 						data: data, // дaнныe для oтпрaвки
-// 						error: function(xhr, status, error) {
-// 							console.log(xhr.responseText + '|\n' + status + '|\n' +error);
-// 						},
-// 						success: function(data){ // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
-// 							var $form = $(this),
-// 								title = $form.data('title'),
-// 								text = $form.data('text'),
-// 								$popup = $('#after-modal'),
-// 								$popupTitle = $popup.find('.js-popup-title'),
-// 								$popupText= $popup.find('.js-popup-text');
-
-
-// 							$popupTitle.text(title);
-// 							$popupText.text(text);
-
-// 							$.magnificPopup.close();
-// 							$.magnificPopup.open({
-// 								showCloseBtn: false,
-// 								items: {
-// 									src: '#after-modal'
-// 								},
-// 								type: 'inline'
-// 								}, 
-// 							0);
-// 						}
-// 					});
-// 					return false;
-// 				});
-// 			}
-// 		});
-// 	});
-// });
-
-// $(function() {
-
-// 	$('.js-gallery').each(function() { // the containers for all your galleries
-// 	    $(this).magnificPopup({
-// 	        delegate: 'a', // the selector for gallery item
-// 	        type: 'image',
-// 	        gallery: {
-// 	          	enabled:true,
-// 	            tPrev: '', // title for left button
-// 				tNext: '', // title for right button
-// 				tCounter: '<span class="mfp-counter">%curr% из %total%</span>' // markup of counter
-// 	        }
-// 	    });
-// 	});
-
-// });
-
-// $(function() {
-
-// 	var $blockWr = $('.js-gallery.portfolio'),
-// 		$block = $blockWr.find('.portfolio__visible'),
-// 		$blockBtn = $blockWr.find('.js-show-more'),
-// 		heightVar = 590;
-
-// 	$blockBtn.click(function(){
-// 		var blockHeight = $block.height();
-// 			blockHeight = blockHeight + heightVar;
-// 			$block.animate({height: blockHeight},400,function(){
-// 				if(blockHeight == 5900){
-// 					$blockBtn.animate({opacity: 0},300);
-// 				}
-// 			});
-		
-// 	});
-
-// });
+$(function() {
+	$('.js-tours-more-btn').click(function(){
+		$(this).toggleClass('active');
+	});
+});
